cbdc13928755e73a4fad4c33368e7c01b15910a3
shahidhj <shahid.h.j@gmail.com>
2019-09-21 23:06:47 -0700

    Array Sum implementation

---
+++ ArraySum.py
@@ -0,0 +1,16 @@
+#Given an array, find the integers that would add upto the sum which is passed as an argument tot he function
+# For example:Given an input ([6,4,3,2,1,7], 9),find the numbers in the array that would add to 9
+
+def hasPairWithSum(array,sums):
+    compliment =0
+    B={}
+    for eachNumber in array:
+        compliment = sums - eachNumber
+        if eachNumber in B.keys():
+            print(compliment,eachNumber)
+            break
+        else:
+            B[compliment] =1
+
+
+hasPairWithSum([6,4,3,2,1,7], 9)
